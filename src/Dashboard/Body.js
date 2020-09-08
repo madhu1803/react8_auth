030d6fe14b22ae0c1343c8b405b3c7f143e1e874
madhumithaa <madhumithaa1411@gmail.com>
2020-09-08 10:05:22 +0530

    dashboard body started

--- src/Dashboard/Body.js
+++ src/Dashboard/Body.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-
+import "./Css/Body.css";
+import { Form, FormControl, Button } from "react-bootstrap";
 export default class Body extends Component {
   render() {
     return (
-      <div>
-        <h1>body</h1>
+      <div className="dashboard-body text-capitalize p-4">
+        <div className="heading-container">
+          <h6>welcome Ms Madhumithaa,</h6>
+          <strong>List of Projects</strong>
+          <Form inline className="search-form">
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="success">Search</Button>
+          </Form>
+        </div>
       </div>
     );
   }
