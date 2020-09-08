b22e49d42280782e6f0d6e599eb099ebdf3d284f
madhumithaa <madhumithaa1411@gmail.com>
2020-09-08 09:56:35 +0530

    header done

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -5,7 +5,6 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>dashboard</h1>
         <Header />
         <Body />
       </div>
--- src/Dashboard/Header.js
+++ src/Dashboard/Header.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { AiOutlineMenu, AiTwotoneBell } from "react-icons/ai";
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>header</h1>
-      </div>
+      <Navbar expand="lg" className="shadow mb-5 bg-white rounded ">
+        <Navbar.Brand href="#home">
+          <AiOutlineMenu style={{ fontSize: "20px" }} className="text-dark" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ml-auto mr-3">
+            <Nav.Link href="#home">
+              <AiTwotoneBell
+                className="text-dark"
+                style={{ fontSize: "20px" }}
+              />
+            </Nav.Link>
+            <NavDropdown
+              title="Madhu"
+              id="basic-nav-dropdown"
+              className="text-dark"
+            ></NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
